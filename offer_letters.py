9fdac12f4d6e2bf9219b11d3c9447444e9a32f01
Meet <meetchampian@gmail.com>
2021-06-17 19:18:50 +0530

    completed offer letter page

--- offer_letters.py
+++ offer_letters.py
@@ -2,6 +2,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.button import MDRoundFlatButton
 from kivymd.uix.dialog import MDDialog
+from kivymd.uix.menu import MDDropdownMenu
 
 class OfferDialog(BoxLayout):
     pass
@@ -10,9 +11,32 @@ class OfferLetters(Screen):
     dialog = None
     def __init__(self, **kw):
         super(OfferLetters, self).__init__(**kw)
+        menu_items = [
+            {
+                "text": "Name",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x="Name": self.menu_callback(x),
+            },
+            {
+                "text": "Company",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x="Company": self.menu_callback(x),
+            },
+            {
+                "text": "Package",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x="Package": self.menu_callback(x),
+            },
+        ]
+        self.menu = MDDropdownMenu(
+            caller = self.ids.dropdown_item,
+            items=menu_items,
+            width_mult=4,
+            max_height = 150
+        )
     
     def full_details(self,row_id):
-        # shows all details of e_resource in a dialog box
+        # shows all details of offer letter in a dialog box
         self.dialog = MDDialog(
             title=str(self.ids[f'{str(row_id)}'].text),
             type="custom",
@@ -33,3 +57,8 @@ class OfferLetters(Screen):
             self.manager.stack.append(self.name)
         else:
             pass
+
+
+    def menu_callback(self, text_item):
+        self.ids.dropdown_item.text = text_item
+        self.menu.dismiss()
