d9ad35e285de3c67f2339773703a8189d5b59d0f
Ishita <ishitajain10037@gmail.com>
2021-06-17 23:34:01 +0530

    changes to placements

--- add_eresources.py
+++ add_eresources.py
@@ -1,11 +1,55 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.picker import MDDatePicker
 from kivymd.uix.picker import MDTimePicker
+from kivymd.uix.menu import MDDropdownMenu
 
 
 class AddEresources(Screen):
-    def _init_(self, **kw):
-        super(AddEresources, self).__init__(**kw)
+    def __init__(self, **kw):
+        super(AddEresources,self).__init__(**kw)
+        menu_items = [
+            {
+                "text": "CSE",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x="CSE": self.menu_callback(x),
+            },
+            {
+                "text": "IT",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x="IT": self.menu_callback(x),
+            },
+            {
+                "text": "ENTC",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x="ENTC": self.menu_callback(x),
+            },
+            {
+                "text": "Civil",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x="Civil": self.menu_callback(x),
+            },
+            {
+                "text": "Mech",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x="Mech": self.menu_callback(x),
+            },
+            {
+                "text": "ELN",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x="ELN": self.menu_callback(x),
+            },
+        ]
+        self.menu = MDDropdownMenu(
+            caller = self.ids.dropdown_item,
+            items=menu_items,
+            width_mult=4,
+            max_height = 300
+        )
+    def menu_callback(self, text_item):
+        self.ids.dropdown_item.text = text_item
+        self.menu.dismiss()
+
+
     def show_date_picker(self):
         date_dialog = MDDatePicker(year=2021,month=2,day=14)
         date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
