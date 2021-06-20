144d9d74edcd0c596fa551ea2b4bfd336a091846
Meet <meetchampian@gmail.com>
2021-06-20 23:59:15 +0530

    added send_mail generalized function

--- database.py
+++ database.py
@@ -3,6 +3,10 @@ from decouple import config
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
 from functools import partial
+from email.message import EmailMessage
+import smtplib
+from decouple import config
+
 
 def db_connector():
     # connects to database
@@ -30,3 +34,19 @@ def disable_toggler(screen,list,value):
     # gets a screen, a list of ids and boolean value and sets diabled value
     for i in list:
         screen.ids[i].disabled = value
+
+def send_mail(screen,subject:str,message:str,to:list):
+    mail = EmailMessage()
+    mail['from'] = config('email')
+    mail['subject'] = subject
+    mail.set_content(message)
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com',465) as server:
+            server.login(config('email'),config('email_password'))
+            for i in to:
+                mail['to'] = i
+                server.send_message(mail)
+                # show_alert_dialog(screen,"Mail sent to all students")
+    except Exception:
+        show_alert_dialog(screen,"Error sending mail to students")
+    
