5287e9d10a2bca5a45c7bb4ba3a3fb1a7e5716bf
Ishita <ishitajain10037@gmail.com>
2021-06-23 17:09:30 +0530

    minor change

--- database.py
+++ database.py
@@ -11,8 +11,8 @@ from heapq import nlargest as _nlargest
 
 def db_connector():
     # connects to database
-    # my_db = MYSQL.connect(host='remotemysql.com',username='PKuyMx1oEg',passwd='DopOx9wsho',database='PKuyMx1oEg', port=3306)
-    my_db = MYSQL.connect(host='localhost',username=config('user'),passwd=config('passwd'),database='wit_tnp')
+    my_db = MYSQL.connect(host='remotemysql.com',username='PKuyMx1oEg',passwd='DopOx9wsho',database='PKuyMx1oEg', port=3306)
+    #my_db = MYSQL.connect(host='localhost',username=config('user'),passwd=config('passwd'),database='wit_tnp')
     my_cursor = my_db.cursor()
     return my_db, my_cursor
 
